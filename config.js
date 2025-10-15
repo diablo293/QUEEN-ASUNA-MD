5b75894083fcdcea45a7a8575a7d51a7b7e13a13
diablo293 <abdounnassirhamidou12@gmail.com>
2025-10-15 08:26:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~WlInXBIC#wdxzYbjPqMH9EU2wMulBdW2QmOLUqpe6ij7o9eg_7ug",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
